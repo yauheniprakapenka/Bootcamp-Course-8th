7a3a3792cc940467c2b515c784bcaec8cb0769e5
Yauheni Prakapenka <eprakopenko@intervale.ru>
2019-12-20 17:17:03 +0300

    Completed task with Eggs

--- BootcampCourse/Controllers/EggsViewController.swift
+++ BootcampCourse/Controllers/EggsViewController.swift
@@ -7,23 +7,47 @@
 //
 
 import UIKit
+import AVFoundation
 
 class EggsViewController: UIViewController {
     
-    let softTime = 3
-    let mediumTime = 6
-    let hardTime = 9
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    private let eggCookingTime = ["Soft": 3, "Medium": 5, "Hard": 7]
+    private var totalTime = 0
+    private var timePassed = 0
+    private var timer = Timer()
+    private var player = AVAudioPlayer()
     
     @IBAction func eggButtonTapped(_ sender: UIButton) {
-        let hardness = sender.currentTitle!
+        timer.invalidate()
         
-        if hardness == "Soft" {
-            print(softTime)
-        } else if hardness == "Medium" {
-            print(mediumTime)
-        } else if hardness == "Hard" {
-            print(hardTime)
+        let hardness = sender.currentTitle!
+        totalTime = eggCookingTime[hardness]!
+
+        progressView.setProgress(0, animated: true)
+        timePassed = 0
+        
+        resultLabel.text = "\(hardness) is cooking..."
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc private func updateTimer() {
+        if timePassed < totalTime {
+            timePassed += 1
+            progressView.setProgress(Float(timePassed) / Float(totalTime), animated: true)
+        } else {
+            timer.invalidate()
+            resultLabel.text = "Eggs cooked 🍳"
+            playAlarm()
         }
     }
     
+    private func playAlarm() {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 }
