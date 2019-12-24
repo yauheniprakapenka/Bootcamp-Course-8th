87aaef32d10831df037d20699bf31e004f3eae1c
Yauheni Prakapenka <eprakopenko@intervale.ru>
2019-12-24 14:12:33 +0300

    Added UI, supporting dark mode

---
+++ BootcampCourse/Controller/ClimatViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ClimatViewController.swift
+//  BootcampCourse
+//
+//  Created by yauheni prakapenka on 24.12.2019.
+//  Copyright © 2019 yauheni prakapenka. All rights reserved.
+//
+
+import UIKit
+
+class ClimatViewController: UIViewController {
+    
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    
+}
