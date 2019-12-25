fb279aa3c5200e46411185f426ead010ecff1445
Yauheni Prakapenka <eprakopenko@intervale.ru>
2019-12-25 17:52:21 +0300

    Parse JSON

---
+++ BootcampCourse/Model/Climat/WeatherData.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherData.swift
+//  BootcampCourse
+//
+//  Created by yauheni prakapenka on 25.12.2019.
+//  Copyright © 2019 yauheni prakapenka. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Decodable {
+    let temp: Double
+}
+
+struct Weather: Decodable {
+    let description: String
+}
--- BootcampCourse/Model/Climat/WeatherManager.swift
+++ BootcampCourse/Model/Climat/WeatherManager.swift
@@ -19,21 +19,29 @@ struct WeatherManager {
     func performRequest(urlString: String) {
         if let url = URL(string: urlString) {
             let session = URLSession(configuration: .default)
-            let task = session.dataTask(with: url, completionHandler: handle(data: response: error: ))
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    self.parseJSON(weatherData: safeData)
+                }
+            }
             task.resume()
         }
     }
     
-    func handle(data: Data?, response: URLResponse?, error: Error?) {
-        if error != nil {
-            print(error!)
-            return
-        }
-        
-        if let safeData = data {
-            let dataString = String(data: safeData, encoding: .utf8)
-            print(dataString)
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodeData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodeData.name)
+            print(decodeData.main.temp)
+            print(decodeData.weather[0].description)
+        } catch {
+            print(error)
         }
     }
 }
-
