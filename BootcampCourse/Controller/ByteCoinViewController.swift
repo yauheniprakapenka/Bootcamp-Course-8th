ff2aa2ffb415c45e4a449fae43846b4f40e8454e
Yauheni Prakapenka <eprakopenko@intervale.ru>
2019-12-27 22:44:22 +0300

    Added picker

--- BootcampCourse/Controller/ByteCoinViewController.swift
+++ BootcampCourse/Controller/ByteCoinViewController.swift
@@ -13,4 +13,38 @@ class ByteCoinViewController: UIViewController {
     @IBOutlet weak var currencyLabel: UILabel!
     @IBOutlet weak var bitcoinLabel: UILabel!
     @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    var coinManager = CoinManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
+    }
 }
+
+//MARK: - UIPickerViewDataSource
+
+extension ByteCoinViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+}
+
+//MARK: - UIPickerViewDelegate
+
+extension ByteCoinViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(coinManager.currencyArray[row])
+    }
+}
+
---
+++ BootcampCourse/Model/ByteCoin/CoinManager.swift
@@ -0,0 +1,14 @@
+//
+//  CoinManager.swift
+//  BootcampCourse
+//
+//  Created by yauheni prakapenka on 27.12.2019.
+//  Copyright © 2019 yauheni prakapenka. All rights reserved.
+//
+
+import Foundation
+
+struct CoinManager {
+    let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+}
