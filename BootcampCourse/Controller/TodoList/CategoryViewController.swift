c800fe1f9cc50dd667c89ed07e0776c11f7ee58f
Yauheni Prakapenka <eprakopenko@intervale.ru>
2020-01-05 09:37:55 +0300

    Added categoryVC, added categoty in core data

---
+++ BootcampCourse/Controller/TodoList/CategoryViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CategoryViewController.swift
+//  BootcampCourse
+//
+//  Created by yauheni prakapenka on 05.01.2020.
+//  Copyright © 2020 yauheni prakapenka. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+    
+    var itemArray = [Item]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func addButtonTapped(_ sender: UIBarButtonItem) {
+        
+    }
+    
+}
