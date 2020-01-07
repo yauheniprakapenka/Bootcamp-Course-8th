549f0c902c22d38ee641962682d9f4d680b4392c
Yauheni Prakapenka <eprakopenko@intervale.ru>
2020-01-07 15:00:37 +0300

    Added dot processing, added +/-, AC, %

--- BootcampCourse/Controller/CalculatorViewController.swift
+++ BootcampCourse/Controller/CalculatorViewController.swift
@@ -12,19 +12,49 @@ class CalculatorViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
-    var isFinishedTypingNumber = true
+    private var isFinishedTypingNumber = true
     
     @IBAction func calcButtonTapped(_ sender: UIButton) {
         isFinishedTypingNumber = true
+        
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Cannot convert display label text to a double")
+        }
+            
+        if let calcMethod = sender.currentTitle {
+            switch calcMethod {
+            case "+/-":
+                displayLabel.text = String("\(number * -1)")
+            case "AC":
+                displayLabel.text = "0"
+            case "%":
+                displayLabel.text = String(number / 100 )
+            default:
+                fatalError("Unknown calc method")
+            }
+        }
     }
     
     @IBAction func numberButtonTapped(_ sender: UIButton) {
+        
         if let numValue = sender.currentTitle {
             
             if isFinishedTypingNumber {
                 displayLabel.text = sender.currentTitle
                 isFinishedTypingNumber = false
             } else {
+                if numValue == "." {
+                    guard let currentDisplayValue = Double(displayLabel.text!) else {
+                        fatalError("Cannot convert display label text to a Double")
+                    }
+                    
+                    let isInt = floor(currentDisplayValue) == currentDisplayValue
+                    
+                    if !isInt {
+                        return
+                    }
+                }
+                
                 displayLabel.text! += numValue
             }
             
