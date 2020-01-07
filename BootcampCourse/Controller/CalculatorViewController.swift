86be871e2326f741a1a9b2cccfb95873114ff946
Yauheni Prakapenka <eprakopenko@intervale.ru>
2020-01-07 10:45:27 +0300

    [calc] cheking is finished typing button

--- BootcampCourse/Controller/CalculatorViewController.swift
+++ BootcampCourse/Controller/CalculatorViewController.swift
@@ -10,4 +10,25 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
     
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    var isFinishedTypingNumber = true
+    
+    @IBAction func calcButtonTapped(_ sender: UIButton) {
+        isFinishedTypingNumber = true
+    }
+    
+    @IBAction func numberButtonTapped(_ sender: UIButton) {
+        if let numValue = sender.currentTitle {
+            
+            if isFinishedTypingNumber {
+                displayLabel.text = sender.currentTitle
+                isFinishedTypingNumber = false
+            } else {
+                displayLabel.text! += numValue
+            }
+            
+        }
+    }
+    
 }
